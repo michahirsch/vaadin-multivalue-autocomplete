5bd64ccdc50a182fe4a46f7dfa756acc322f57e7
Michael Hirsch <michael.hirsch@bosch-si.com>
2016-08-24 15:38:45 +0200

    add dummy suggest oracle to bean configuration
    Signed-off-by: Michael Hirsch <michael.hirsch@bosch-si.com>

--- vaadin-suggestion-example/src/main/java/com/bosch/example/DemoAppApplication.java
+++ vaadin-suggestion-example/src/main/java/com/bosch/example/DemoAppApplication.java
@@ -2,7 +2,11 @@ package com.bosch.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.bosch.example.rsql.SuggestContext;
+import com.bosch.example.rsql.SuggestOracle;
+import com.bosch.example.rsql.TokenSuggestionContext;
 import com.vaadin.spring.annotation.EnableVaadin;
 
 @SpringBootApplication
@@ -11,5 +15,35 @@ public class DemoAppApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(DemoAppApplication.class, args);
+    }
+
+    @Bean
+    public SuggestOracle suggestOracle() {
+        return new DummySuggestOracle();
+    }
+
+    private static final class DummySuggestOracle implements SuggestOracle {
+
+        @Override
+        public SuggestContext getSuggestion(final String query, final int cursorPos) {
+            return new SuggestContext() {
+
+                @Override
+                public boolean hasSyntaxError() {
+                    return false;
+                }
+
+                @Override
+                public TokenSuggestionContext getSyntaxErrorContext() {
+                    return null;
+                }
+
+                @Override
+                public TokenSuggestionContext getCursorContext() {
+                    return null;
+                }
+            };
+        }
+
     }
 }
--- vaadin-suggestion-example/src/main/java/com/bosch/example/ui/ExampleView.java
+++ vaadin-suggestion-example/src/main/java/com/bosch/example/ui/ExampleView.java
@@ -1,8 +1,8 @@
 package com.bosch.example.ui;
 
-import com.bosch.example.rsql.SuggestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.bosch.example.rsql.SuggestOracle;
-import com.bosch.example.rsql.TokenSuggestionContext;
 import com.bosch.example.ui.component.AutocompleteTextfield;
 import com.bosch.example.ui.component.TextFieldSuggestionBox;
 import com.vaadin.navigator.View;
@@ -15,43 +15,18 @@ public class ExampleView extends VerticalLayout implements View {
 
     private static final long serialVersionUID = 1L;
 
+    @Autowired
+    private SuggestOracle suggestOracle;
+
     @Override
     public void enter(final ViewChangeEvent event) {
 
         this.setSizeFull();
 
         final AutocompleteTextfield textField = new AutocompleteTextfield();
-        new TextFieldSuggestionBox(textField, getSuggestOracle());
+        new TextFieldSuggestionBox(textField, suggestOracle);
         textField.setWidth("800px");
         addComponent(textField);
     }
 
-    private SuggestOracle getSuggestOracle() {
-        return new DummySuggestOracle();
-    }
-
-    private static final class DummySuggestOracle implements SuggestOracle {
-
-        @Override
-        public SuggestContext getSuggestion(final String query, final int cursorPos) {
-            return new SuggestContext() {
-
-                @Override
-                public boolean hasSyntaxError() {
-                    return false;
-                }
-
-                @Override
-                public TokenSuggestionContext getSyntaxErrorContext() {
-                    return null;
-                }
-
-                @Override
-                public TokenSuggestionContext getCursorContext() {
-                    return null;
-                }
-            };
-        }
-
-    }
 }
